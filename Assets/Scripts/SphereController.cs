24276cdab313a0be450ec9e1b251b26fa7b9d349
diffran15 <diffran5@gmail.com>
2021-10-10 22:33:58 +0700

    Problem-3

---
+++ Assets/Scripts/SphereController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour
+{
+    public float speed = 3f;
+
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+    }
+}
